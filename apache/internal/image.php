e56304ce4ac890bdd904142a46dcf369536f6763
nicothebrush <nicola.riolini@gmail.com>
2017-02-13 11:03:44 +0100

    debug

--- apache/internal/image.php
+++ apache/internal/image.php
@@ -6,9 +6,9 @@
     //if (file_exists($name)) {
         // send the right headers
         header("Content-Type: image/jpeg");
-        header("Content-Length: " . filesize($name));
-        fpassthru($name);
-        ////readfile($name);
+        //header("Content-Length: " . filesize($name));
+        //fpassthru($name);
+        readfile($name);
         //imagejpeg($name); // dump the picture and stop the script
         //imagedestroy($name); // Free up memory
         exit;
