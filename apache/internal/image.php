bbfa1cd41170c26110e45a3bdc73756c423e2b11
nicothebrush <nicola.riolini@gmail.com>
2017-02-13 10:35:11 +0100

    debug

--- apache/internal/image.php
+++ apache/internal/image.php
@@ -2,7 +2,7 @@
     // open the file in a binary mode:
     $name = './images/Varie Roberta/FOTO OK/SCONTORNATE.THUMB/541675.png';
     $fp = fopen($name, 'rb');
-
+    die($name);
     if (file_exists($name)) {
         // send the right headers
         header("Content-Type: image/jpg");
