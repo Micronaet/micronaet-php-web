3306c3d7d71b1f6bcec3bb0310229cefd101bee6
nicothebrush <nicola.riolini@gmail.com>
2017-02-13 11:03:20 +0100

    debug

--- apache/internal/image.php
+++ apache/internal/image.php
@@ -2,7 +2,6 @@
     // open the file in a binary mode:
     $code = $_GET['code'];
     $name = "./images/$code.jpg";
-    die($name);
     $fp = fopen($name, 'rb');
     //if (file_exists($name)) {
         // send the right headers
