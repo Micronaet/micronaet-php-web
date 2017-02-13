55420c9e1ee29e6fd5dc30f2207d93177640d69b
nicothebrush <nicola.riolini@gmail.com>
2017-02-13 10:38:28 +0100

    debug

--- apache/internal/image.php
+++ apache/internal/image.php
@@ -2,7 +2,7 @@
     // open the file in a binary mode:
     $name = './images/541675.jpg';
     $fp = fopen($name, 'rb');
-    if (file_exists($name)) {
+    //if (file_exists($name)) {
         // send the right headers
         header("Content-Type: image/jpg");
         header("Content-Length: " . filesize($name));
@@ -10,6 +10,6 @@
         // dump the picture and stop the script
         fpassthru($fp);
         exit;
-        }        
+    //    }        
 ?>
 Immagine non trovata!
