3447ce64eb6c5667d686da6aa161c538a8297b60
nicothebrush <nicola.riolini@gmail.com>
2017-02-13 10:27:32 +0100

    image page for testing

--- apache/internal/image.php
+++ apache/internal/image.php
@@ -1,11 +1,11 @@
 <?php
     // open the file in a binary mode:
-    $name = './img/ok.png';
+    $name = './images/Varie Roberta/FOTO OK/SCONTORNATE.THUMB/541675.png';
     $fp = fopen($name, 'rb');
 
     if (file_exists($name)) {
         // send the right headers
-        header("Content-Type: image/png");
+        header("Content-Type: image/jpg");
         header("Content-Length: " . filesize($name));
 
         // dump the picture and stop the script
