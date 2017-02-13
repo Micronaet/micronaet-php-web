3a4157871cfa077d6437e6e4f0309369bf030dde
nicothebrush <nicola.riolini@gmail.com>
2017-02-13 11:06:53 +0100

    debug

--- apache/internal/image.php
+++ apache/internal/image.php
@@ -4,13 +4,15 @@
     $name = "./images/$code.jpg";
     $fp = fopen($name, 'rb');
     //if (file_exists($name)) {
-        // send the right headers
+        // Mode 1:
         header("Content-Type: image/jpg");
-        //header("Content-Length: " . filesize($name));
+        header("Content-Length: " . filesize($name));
         //fpassthru($name);
-        //readfile($name);
-        imagejpeg($name); // dump the picture and stop the script
-        imagedestroy($name); // Free up memory
+        readfile($name);
+        
+        //Mode 3:
+        //imagejpeg($name); // dump the picture and stop the script
+        //imagedestroy($name); // Free up memory
         exit;
     //    }        
 ?>
