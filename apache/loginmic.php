ce86c939b2143f65e040a4e22b76b0bdc81ff941
nicothebrush <nicola.riolini@gmail.com>
2017-03-28 16:17:44 +0200

    add user

--- apache/loginmic.php
+++ apache/loginmic.php
@@ -46,6 +46,9 @@ if(isset($_POST['user']) && isset($_POST['password'])){
     $utenti[] = array(
 	    'user' => 'dispo', 'passwd' => 'fiamgpb', 'admin' => false,
     );
+    $utenti[] = array(
+	    'user' => 'hanne', 'passwd' => 'HCebbesen', 'admin' => false,
+    );
     $_SESSION['admin'] = true;
     foreach($utenti as $u){
 	    if($_POST['user'] == $u['user'] && $_POST['password'] == $u['passwd']) {
