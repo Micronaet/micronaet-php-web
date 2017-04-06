11a223e489dc1e7d0d77ccc5a1a369934f7f5a6e
nicothebrush <nicola.riolini@gmail.com>
2017-04-06 12:11:30 +0200

    fast git

--- apache/find.php
+++ apache/find.php
@@ -7,7 +7,7 @@
     $ips = array("91.187.199.104", "91.187.199.105", "91.187.199.106", "91.187.199.107", "79.60.135.198");
 
     $location = '-';
-    $shop = isset($_GET['shop']) ? $_GET['shop'] == 't' : false;
+    $shop = isset($_GET['shop']) ? $_GET['shop'] == '1' : '0';
 
     if(!isset($_SESSION['logged'])){
         if(!in_array($_SERVER['REMOTE_ADDR'], $ips)) {
